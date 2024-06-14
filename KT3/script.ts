4c90cce76a6acbf9b1f1ff627d529a2edd6424df
Chett4 <129016266+Chett4@users.noreply.github.com>
2024-06-14 20:25:59 +1000

    Update script.ts

--- KT3/script.ts
+++ KT3/script.ts
@@ -1,75 +1,143 @@
-interface Logger {
-    log(message: string): void;
-}
-class Plotter {
-    private position: { x: number; y: number };
-    private angle: number;
-    private isCarriageDown: boolean;
-    private color: string;
-    private logger: Logger;
+class Ploter {
+  plotterState: PlotterState
 
-    constructor(logger: Logger) {
-        this.position = { x: 0, y: 0 };
-        this.angle = 0;
-        this.isCarriageDown = false;
-        this.color = 'black';
-        this.logger = logger;
-    }
-    move(distance: number): void {
-        const newX = this.position.x + distance * Math.cos(this.angle * (Math.PI / 180));
-        const newY = this.position.y + distance * Math.sin(this.angle * (Math.PI / 180));
-        const from = `(${this.position.x}, ${this.position.y})`;
-        const to = `(${newX}, ${newY})`;
-        const lineInfo = `Чертим линию из ${from} в ${to} используя ${this.color} цвет.`;
-        const moveInfo = `Передвигаем на ${distance} от точки ${from}.`;
+  drawLine(prt: Printer, from: Position, to: Position, color: LineColor): void {
+    prt(`...Чертим линию из (${from.x}, ${from.y}) в (${to.x}, ${to.y}) используя ${color} цвет.`);
+  }
 
-        this.logger.log(this.isCarriageDown ? lineInfo : moveInfo);
+  calcNewPosition(distance: Distance, angle: Angle, current: Position): Position {
+    const angle_in_rads = angle * (Math.PI / 180.0) * 1.0;
+    const x = current.x + distance * Math.cos(angle_in_rads);
+    const y = current.y + distance * Math.sin(angle_in_rads);
+    //Возращаем новую позицию
+    const position = { x: Math.round(x), y: Math.round(y) }
+    this.plotterState.position = position
+    return position
+  }
+  move(prt: Printer, distance: Distance, state: PlotterState): PlotterState {
+    let newPosition = this.calcNewPosition(distance, state.angle, state.position);
+    if (state.carriageState === CarriageState.DOWN) {
+      this.drawLine(prt, state.position, newPosition, state.color);
+    } else {
+      prt(`Передвигаем на ${distance} от точки (${state.position.x}, ${state.position.y})`);
+    }
+    const retState = { ...state };
+    retState.position = newPosition;
+    this.plotterState = retState;
+    return retState;
+  }
+  turn(prt: Printer, angle: Angle, state: PlotterState): PlotterState {
+    prt(`Поворачиваем на ${angle} градусов`);
+    const newAngle = (state.angle + angle) % 360.0;
+    const retState = { ...state };
+    retState.angle = newAngle;
+    this.plotterState = retState;
+    return retState;
 
-        this.position.x = newX;
-        this.position.y = newY;
-    }
-
-    turn(degrees: number): void {
-        this.logger.log(`Поворачиваем на ${degrees} градусов.`);
-        this.angle += degrees;
-    }
-
-    carriageDown(): void {
-        this.logger.log('Опускаем каретку.');
-        this.isCarriageDown = true;
-    }
-
-    carriageUp(): void {
-        this.logger.log('Поднимаем каретку.');
-        this.isCarriageDown = false;
-    }
-
-    setColor(color: string): void {
-        this.logger.log(`Устанавливаем ${color} цвет линии.`);
-        this.color = color;
-    }
-
-    setPosition(x: number, y: number): void {
-        this.logger.log(`Устанавливаем позицию каретки в (${x}, ${y}).`);
-        this.position.x = x;
-        this.position.y = y;
-    }
-}
-class LogToConsole implements Logger {
-    log(message: string): void {
-        console.log(message);
-    }
-}
-function drawTriangle(plt: Plotter, size: number): void {
-    plt.setColor('Green');
-    plt.carriageDown();
+  }
+  carriageUp(prt: Printer, state: PlotterState): PlotterState {
+    prt("Поднимаем каретку");
+    const retState = { ...state };
+    retState.carriageState = CarriageState.UP;
+    this.plotterState = retState;
+    return retState;
+  }
+  carriageDown(prt: Printer, state: PlotterState): PlotterState {
+    prt("Опускаем каретку");
+    const retState = { ...state };
+    retState.carriageState = CarriageState.DOWN;
+    this.plotterState = retState;
+    return retState;
+  }
+  setColor(prt: Printer, color: LineColor, state: PlotterState): PlotterState {
+    prt(`Устанавливаем ${color} цвет линии.`);
+    const retState = { ...state };
+    retState.color = color;
+    this.plotterState = retState;
+    return retState;
+  }
+  setPosition(prt: Printer, position: Position, state: PlotterState): PlotterState {
+    prt(`Устанавливаем позицию каретки в (${position.x}, ${position.y}).`);
+    const retState = { ...state };
+    retState.position = position;
+    this.plotterState = retState;
+    return retState;
+  }
+  drawTriangle(prt: Printer, size: number, state: PlotterState): PlotterState {
+    state = this.carriageDown(prt, state);
     for (let i = 0; i < 3; ++i) {
-        plt.move(size);
-        plt.carriageUp();
-        plt.turn(120.0);
-        plt.carriageDown();
+      state = this.move(prt, size, state);
+      state = this.turn(prt, 120.0, state);
     }
-    plt.carriageUp();
+    return this.carriageUp(prt, state);
+  }
+  drawSquare(prt: Printer, size: number, state: PlotterState): PlotterState {
+    state = this.carriageDown(prt, state);
+    for (let i = 0; i < 4; ++i) {
+      state = this.move(prt, size, state);
+      state = this.turn(prt, 90.0, state);
+    }
+    return this.carriageUp(prt, state);
+  }
 }
-const plotter = new Plotter(new LogToConsole());
-drawTriangle(plotter, 100.0);
+
+
+class LogToConsole implements Logger {
+  log(message: string): void {
+
+  }
+}
+
+interface Logger {
+  log(message: string): void;
+}
+
+/**
+ * Объявление типов
+ */
+
+type Point = number;
+type Distance = number;
+type Angle = number;
+type Position = { x: Point; y: Point };
+enum CarriageState {
+  UP,
+  DOWN
+}
+enum LineColor {
+  BLACK = "чёрный",
+  RED = "красный",
+  GREEN = "зелёный"
+}
+type PlotterState = {
+  position: Position;
+  angle: Angle;
+  color: LineColor;
+  carriageState: CarriageState;
+};
+type Printer = (s: string) => void;
+
+const printer: Printer = console.log;
+
+function initializePlotterState(position: Position, angle: Angle, color: LineColor, carriageState: CarriageState): PlotterState {
+  return {
+    position: position,
+    angle: angle,
+    color: color,
+    carriageState: carriageState
+  };
+}
+
+let initPosition: Position = { x: 0.0, y: 0.0 };
+let initColor: LineColor = LineColor.BLACK;
+let initAngle: Angle = 0.0;
+let initCarriageState: CarriageState = CarriageState.UP;
+
+let plotter: Ploter = new Ploter();
+
+let plotterState = initializePlotterState(initPosition, initAngle, initColor, initCarriageState);
+
+plotter.drawTriangle(printer, 100.0, plotterState);
+plotter.setPosition(printer, {x:10.0, y:10.0}, plotterState);
+plotter.setColor(printer, LineColor.RED , plotterState);
+plotter.drawSquare(printer, 80.0, plotterState);
